467a52aad4cf5d9813add99ef41141851c2c11e5
DEV_5 <vasudevvedusav@gmail.com>
2020-05-03 17:55:46 +0530

    added basic code module code to USB.c

--- USB.c
+++ USB.c
@@ -1,6 +1,17 @@
 //my first drivers program
-#include<stdio.h>
-int main()
-{
-    printf("hello this is a print statement\n");
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int hello_init(void){
+    printk(KERN_ALERT "Test: Hello world this is dev\n");
+
+    return 0;
 }
+static int hello_exit(void){
+    printk(KERN_ALERT "Test: GOODBYE from dev\n");
+    return 0;
+
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
